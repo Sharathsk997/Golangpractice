91b15b842c1c77797ae68f1c82b708fbf60dd84b
Sharathsk997 <Sharathsk997@gmail.com>
2021-06-14 10:05:59 +0530

    continue

--- Hello.go
+++ Hello.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("haha")
-	fmt.Println("helloo Golang")
+	fmt.Println("helloo Golang") //sequential
+	foo()
 
+	fmt.Printf("exited foo")
+}
+
+func foo() {
+	for i := 0; i < 20; i++ { //loop
+		if i%2 == 0 { //conditional
+			fmt.Println(i)
+		}
+	}
 }
